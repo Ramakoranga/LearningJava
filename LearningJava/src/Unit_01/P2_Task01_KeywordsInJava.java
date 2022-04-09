05e1b14a7820dbfd2f77d69b9c92017f235a6ca5
RAMA KORANGA <RAMA KORANGA@LAPTOP-39OVE2K7>
2022-04-09 14:12:05 +0530

    second commit

--- LearningJava/src/Unit_01/P2_Task01_KeywordsInJava.java
+++ LearningJava/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -4,7 +4,24 @@ public class P2_Task01_KeywordsInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Syntax error on token "int", invalid VariableDeclaratorId
+		double int = 10;
+		
+//		Syntax error on token "int", delete this token
+		P2_Task01_KeywordsInJava int = new P2_Task01_KeywordsInJava();
+		
+	}
+//	Syntax error on token "double", Identifier expected
+	void double() {
+		
+	}
+	
+//	Syntax error on token "int", Identifier expected
+	class int() {
+		
+	}
+
 
 	}
 
-}
+
