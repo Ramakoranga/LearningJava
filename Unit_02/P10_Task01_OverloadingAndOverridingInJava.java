f5cfa8a5f58d0a9e177470d32fb434cde7892da0
Ramakoranga <96382603+Ramakoranga@users.noreply.github.com>
2022-04-16 09:52:41 +0530

    Add files via upload

---
+++ Unit_02/P10_Task01_OverloadingAndOverridingInJava.java
@@ -0,0 +1,10 @@
+package Unit_02;
+
+class P10_Task01_OverloadingAndOverridingInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Unit_02/P12_Task01_InterfaceInJavaimplementsAnimal.java
@@ -0,0 +1,10 @@
+package Unit_02;
+
+public class P12_Task01_InterfaceInJavaimplementsAnimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
