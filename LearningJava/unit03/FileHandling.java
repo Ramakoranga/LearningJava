f428838eb73b1e86cb77e69bb8766f5bf6886c60
Ramakoranga <96382603+Ramakoranga@users.noreply.github.com>
2022-06-04 09:34:19 +0530

    Add files via upload

---
+++ LearningJava/unit03/FileHandling.java
@@ -0,0 +1,15 @@
+package unit03;
+
+public class FileHandling {
+
+	public static void main(String[] args) {
+		// input is taken from file not from console 
+		//  output is not in console 
+		//readLine () is in bufferedReader class 
+		//bufferedReader class does not have default constructor
+		//file reader figure out the 
+       // current time and date
+		//how to draw log using java 
+	}
+
+}
